43e025d647718362f2ead00ae152e0e6b6506c7a
Fuseteam <fusekai@outlook.com>
2020-08-30 22:37:25 -0300

    extract the model out of pdf.php

--- www/pdf.php
+++ www/pdf.php
@@ -1,36 +1,6 @@
 <?php
-require_once ('base/data.php');
-require('fpdf/html_table.php');
-class PDF extends HPDF{
-    var $heading,//Heading shown at the top of the document
-        $title;//title of the document
-    function Header(){
-        $this->SetFont('Arial', 'B', 16);
-        $this->Cell(40,10,$this->heading);
-        $this->Ln(20);
-    }
-    function Footer(){
-        $this->SetY(-15);
-        $this->SetFont('Arial', 'I', 8);
-        $this->AliasNbPages();
-        $this->Cell(0,10,$this->PageNo(),0,0,'C');
-    }
+require('model/pdf.php');
 
-    function infoRow($w, $c1, $c2){
-        $oy=$this->GetY();
-        $ox=$this->GetX();
-        $this->Cell($w, 4, $c1, 0, 1);
-        $this->SetXY($ox+$w, $oy);
-        $this->Cell($w, 4, $c2, 0, 1);
-    }
-
-    function setTitlePDF($title){
-        $this->SetTitle($title);
-        $title=str_replace(' ', '_', $title);
-        $title=str_replace('_-_', '-', $title);
-        $this->title=$title.'.pdf';
-    }
-}
 $pdf = new PDF('L');
 $pdf->AddPage();
 $pdf->SetFont('Arial', '', 8);
