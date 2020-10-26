1969d788299605b65e113dce14ddb356bc25a9dd
Fuseteam <fusekai@outlook.com>
2020-10-26 09:30:40 -0300

    adjust webapp to the new directory structure

--- www/controller/autoloader.php
+++ www/controller/autoloader.php
@@ -1,26 +1,30 @@
 <?php
 session_start();
     // {path}/autoloader.php
-    function loadClass($className) {
-        $fileName = '';
-        $namespace = '';
+function getIncludePath($i) {
 
-        // Sets the include path as the "src" directory
-        $includePath = get_include_path().DIRECTORY_SEPARATOR;
+    $includePaths=explode(PATH_SEPARATOR, get_include_path());
+    return $includePaths[$i];
+}
 
-        if (false !== ($lastNsPos = strripos($className, '\\'))) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        $fileName .= $className . '.php';
-        $fullFileName = $includePath . $fileName;
-
-        if (file_exists($fullFileName)) {
-            require $fullFileName;
-        } else {
-            echo 'Class "'.$className.'" does not exist.';
-        }
+function loadClass($className){
+    $fileName = '';
+    $namespace = '';
+    // Sets the include path as the "src" directory
+    $includePath = getIncludePath(1).DIRECTORY_SEPARATOR;
+    if (false !== ($lastNsPos = strripos($className, '\\'))) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
+    $fileName .= $className . '.php';
+    $fullFileName = $includePath . $fileName;
+
+    if (file_exists($fullFileName)) {
+        require $fullFileName;
+    } else {
+        echo 'Class "'.$className.'" does not exist.';
+    }
+}
     spl_autoload_register('loadClass'); // Registers the autoloader
 ?>
